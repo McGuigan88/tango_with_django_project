65aaa95fc688581d641182846e039ec9a258be49
McGuigan88 <60005842+McGuigan88@users.noreply.github.com>
2020-01-31 16:10:26 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<a href = '/rango/about/'>About</a> Rango says hey there partner!")
+    return HttpResponse("<a href = '/rango/about/'>About</a>Rango says hey there partner!")
 
 def about(request):
     return HttpResponse("<a href = '/rango/index/'>index</a>Rango says here is the about page")
